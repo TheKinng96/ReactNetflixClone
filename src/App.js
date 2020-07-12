225db77a069a04a23b201cfa1823891f85b83eb4
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-12 17:04:35 +0900

    done adding axios to get data from tmdb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Row from './components/Row-components';
+import requests from './API/request';
 
 function App() {
   return (
     <div className="App">
       <h1>Ready</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+
     </div>
   );
 }
